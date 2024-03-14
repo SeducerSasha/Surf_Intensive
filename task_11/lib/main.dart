69d71b359218ff3fcd250c1096db2f216aa2b94d
Sasha <seducer@bk.ru>
2024-03-14 23:01:46 +0300

    Выполнение задания Визитка

--- task_11/lib/main.dart
+++ task_11/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,214 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Business Card',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BusinessCard(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SingleChildScrollView(
+          child: Column(
+            /// Всю информацию выводим в колонке.
+            children: [
+              SizedBox(
+                height: 5,
+              ),
+
+              /// Вывод аватара.
+              Avatar(),
+              SizedBox(
+                height: 10,
+              ),
+
+              /// Вывод ФИО.
+              Text(
+                'Иванов Александр Валерьевич',
+                style: TextStyle(fontFamily: 'Pacifico', fontSize: 24),
+              ),
+
+              /// Вывод блоков информации.
+              Column(
+                children: [
+                  BlocCard(
+                    headerText: 'О себе',
+                    fontFamilyHeader: 'BadScript',
+                    imageName: 'assets/dinosaur.svg',
+                    isSVG: true,
+                    blocText:
+                        'Я динозавр! Да, да!\nТак и есть, ведь я уже разменял полвека :-)',
+                    fontFamilyText: 'Amatic',
+                    colorCard: Colors.white70,
+                    fontSizeText: 24,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  BlocCard(
+                    headerText: 'Увлечения',
+                    fontFamilyHeader: 'BadScript',
+                    imageName: 'assets/hobby.png',
+                    isSVG: false,
+                    blocText:
+                        'Обожаю программирование! Это моя самая любимая работа!',
+                    fontFamilyText: 'HandJet',
+                    colorCard: Colors.orange,
+                    fontSizeText: 21,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  BlocCard(
+                    headerText: 'Опыт',
+                    fontFamilyHeader: 'BadScript',
+                    imageName: 'assets/practice.jpg',
+                    isSVG: false,
+                    blocText:
+                        'Опыта с Flutter нет! Но дико хочется! К тому же, я хоть и динозавр, но жажда знаний не кончается. Для драконов сокровище - золото, для меня - знания!',
+                    fontFamilyText: 'Kablammo',
+                    colorCard: Colors.lime,
+                    fontSizeText: 14,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+/// Определяем шаблон декорирования, тень и цвет карточки информации.
+BoxDecoration decorationCard(final color) {
+  return BoxDecoration(
+    color: color,
+    borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+        bottomLeft: Radius.circular(10),
+        bottomRight: Radius.circular(10)),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: const Offset(0, 3), // changes position of shadow
+      ),
+    ],
+  );
+}
+
+/// Блок вывода информации
+class BlocCard extends StatelessWidget {
+  final String headerText;
+  final String imageName;
+  final bool isSVG;
+  final String blocText;
+  final String fontFamilyHeader;
+  final String fontFamilyText;
+  final Color colorCard;
+  final double fontSizeText;
+  const BlocCard({
+    super.key,
+    required this.headerText,
+    required this.imageName,
+    required this.blocText,
+    required this.isSVG,
+    required this.fontFamilyHeader,
+    required this.fontFamilyText,
+    required this.colorCard,
+    required this.fontSizeText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            headerText,
+            style: TextStyle(
+                fontFamily: fontFamilyHeader,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: decorationCard(colorCard),
+            child: Row(
+              children: [
+                isSVG
+                    ? SvgPicture.asset(
+                        imageName,
+                        width: 100,
+                        height: 80,
+                      )
+                    : Image.asset(
+                        imageName,
+                        width: 100,
+                        height: 80,
+                        fit: BoxFit.fitHeight,
+                      ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  child: Text(
+                    blocText,
+                    style: TextStyle(
+                      fontFamily: fontFamilyText,
+                      fontSize: fontSizeText,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    softWrap: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Avatar extends StatelessWidget {
+  const Avatar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: 100,
+        height: 100,
+        child: CircleAvatar(
+          backgroundImage: AssetImage('assets/avatar.png'),
+        ),
+      ),
     );
   }
 }
